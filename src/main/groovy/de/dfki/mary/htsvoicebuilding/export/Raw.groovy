d3c5dbc4c797509c91707d53d65bc9d21fd7b90f
Sebastien Le Maguer <lemagues@tcd.ie>
2018-10-07 17:00:52 +0100

    export: add raw proper task management class

--- src/main/groovy/de/dfki/mary/htsvoicebuilding/export/Raw.groovy
+++ src/main/groovy/de/dfki/mary/htsvoicebuilding/export/Raw.groovy
@@ -6,114 +6,161 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import org.apache.commons.io.FileUtils
 
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.MavenPlugin
+import org.gradle.api.tasks.Copy
+import org.gradle.api.tasks.Exec
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.bundling.Zip
 
-class Raw {
-    def static export(project) {
 
-        def user_configuration = project.configuration.user_configuration
-        def export_dir = project.buildDir
-        def trained_files = project.trained_files
+class ExportRAW
+{
+    public static void addTasks(Project project)
+    {
+        def last_cluster = project.user_configuration.settings.training.nb_clustering - 1
+        def export_dir = "$project.buildDir/raw"
 
-        // Define directories
-        def model_dir = "$export_dir/raw/models"
-        def tree_dir = "$export_dir/raw/trees"
-        (new File(model_dir)).mkdirs()
-        (new File(tree_dir)).mkdirs()
+        project.task("exportRAWTrees") {
+            doLast {
+                def tree_dir = new File("$export_dir/trees")
+                tree_dir.mkdirs()
 
-        // Copy model files
-        project.copy {
-            from project.property("trainClusteredModels${user_configuration.settings.training.nb_clustering - 1}").trained_cmp_file
-            into model_dir
-            rename { file -> "re_clustered_cmp.mmf" }
-        }
+                project.configuration.user_configuration.models.cmp.streams.each { stream ->
+                    project.copy {
+                        from "${project.tree_dir}/${stream.name}.${last_cluster}.inf"
+                        into tree_dir
+                        rename { file -> "${stream.name}.inf"}
+                    }
+                }
 
-        project.copy {
-            from project.property("trainClusteredModels${user_configuration.settings.training.nb_clustering - 1}").trained_dur_file
-            into model_dir
-            rename { file -> "re_clustered_dur.mmf" }
-        }
-
-        // Copy tree files
-        user_configuration.models.cmp.streams.each { stream ->
-            project.copy {
-                from trained_files.get(stream.name + "_tree")
-                into tree_dir
-                rename { file -> "${stream.name}.inf"}
+                project.copy {
+                    from "${project.tree_dir}/dur.${last_cluster}.inf"
+                    into tree_dir
+                    rename { file -> "dur.inf"}
+                }
             }
         }
 
-        project.copy {
-            from trained_files.get("dur_tree")
-            into tree_dir
-            rename { file -> "dur.inf"}
-        }
+        project.task("exportRAWHMMModels") {
+            doLast {
+                def model_dir = new File("$export_dir/models")
+                model_dir.mkdirs()
 
-        // Copy list
-        project.copy {
-            from project.generateFullList.list_file
-            into "$export_dir/raw"
-            rename { file -> "full.list" }
-        }
-
-
-        // Copy gv informations
-        if (user_configuration.gv.use) {
-            def gv_dir = "$export_dir/raw/gv"
-            (new File(gv_dir)).mkdirs()
-
-            // Trees
-            user_configuration.models.cmp.streams.each { stream ->
+                // Copy model files
                 project.copy {
-                    from trained_files.get(stream.name + "_tree_gv")
+                    from project.property("trainClusteredModels${last_cluster}").trained_cmp_file
+                    into model_dir
+                    rename { file -> "re_clustered_cmp.mmf" }
+                }
+
+                project.copy {
+                    from project.property("trainClusteredModels${last_cluster}").trained_dur_file
+                    into model_dir
+                    rename { file -> "re_clustered_dur.mmf" }
+                }
+            }
+        }
+
+        project.task("exportRAWLists") {
+            doLast {
+                project.copy {
+                    from project.full_list_filename
+                    into "$export_dir/raw"
+                    rename { file -> "full.list" }
+                }
+            }
+        }
+
+        project.task("exportRAWGV") {
+            doLast {
+                def gv_dir = new File("$export_dir/gv")
+                gv_dir.mkdirs()
+
+                // Trees
+                project.configuration.user_configuration.models.cmp.streams.each { stream ->
+                    project.copy {
+                        from "${project.gv_dir}/${stream.name}.inf"
+                        into gv_dir
+                        rename { file -> "${stream.name}.inf"}
+                    }
+                }
+
+                // Model
+                project.copy {
+                    from project.trainGVClustered.trained_model_file
                     into gv_dir
-                    rename { file -> "${stream.name}.inf"}
+                    rename { file -> "clustered.mmf"}
                 }
-            }
 
-            // Model
-            project.copy {
-                from project.trainGVClustered.trained_model_file
-                into gv_dir
-                rename { file -> "clustered.mmf"}
-            }
-
-            // List
-            project.copy {
-                from project.generateGVListFile.list_file
-                into gv_dir
-                rename { file -> "gv.list"}
-            }
-        }
-
-        // Copy windows
-        (new File("$export_dir/raw/win")).mkdirs()
-        user_configuration.models.cmp.streams.each { stream ->
-            stream.winfiles.each { winfilename ->
+                // List
                 project.copy {
-                    from winfilename
-                    into "$export_dir/raw/win/"
-                    rename { file -> (new File(winfilename)).getName() }
+                    from project.generateGVListFile.list_file
+                    into gv_dir
+                    rename { file -> "gv.list"}
                 }
             }
         }
 
-        // Copy DNN part (TODO)
-        if ((user_configuration.settings.training.kind) &&
-            (user_configuration.settings.training.kind.equals("dnn")))
-        {
-            FileUtils.copyDirectory(project.trainDNN.model_dir.getAsFile().get(),
-                                    new File("$export_dir/raw/DNN/models"));
+        project.task("exportRAWWindows") {
+            doLast {
 
-            FileUtils.copyDirectory(new File("$project.buildDir/dnn/var"),
-                                    new File("$export_dir/raw/DNN/var"));
-
-            Files.copy(Paths.get(user_configuration.settings.dnn.qconf),
-                       Paths.get("$export_dir/raw/DNN/qconf.conf"));
+                (new File("$export_dir/win")).mkdirs()
+                project.configuration.user_configuration.models.cmp.streams.each { stream ->
+                    stream.winfiles.each { winfilename ->
+                        project.copy {
+                            from winfilename
+                            into "$export_dir/win/"
+                            rename { file -> (new File(winfilename)).getName() }
+                        }
+                    }
+                }
+            }
         }
 
-        // Adapt configuration file and expose it
-        def export_configuration = user_configuration
-        export_configuration.remove("data")
-        (new File("$export_dir/raw/config.json")).text = new JsonBuilder(export_configuration).toPrettyString()
+
+        project.task("exportRAWDNNModels") {
+            doLast {
+                FileUtils.copyDirectory(project.trainDNN.model_dir.getAsFile().get(),
+                                        new File("$export_dir/raw/DNN/models"));
+
+                FileUtils.copyDirectory(new File("$project.buildDir/dnn/var"),
+                                        new File("$export_dir/raw/DNN/var"));
+
+                Files.copy(Paths.get(project.configuration.user_configuration.settings.dnn.qconf),
+                           Paths.get("$export_dir/raw/DNN/qconf.conf"));
+            }
+        }
+
+
+        project.task("exportRAWConfiguration") {
+            doLast {
+                // Adapt configuration file and expose it
+                def export_configuration = project.configuration.user_configuration
+                export_configuration.remove("data")
+                (new File("$export_dir/raw/config.json")).text = new JsonBuilder(export_configuration).toPrettyString()
+            }
+        }
+
+        project.task("exportRAW") {
+            dependsOn project.exportRAWTrees
+            dependsOn project.exportRAWHMMModels
+            dependsOn project.exportRAWWindows
+            dependsOn project.exportRAWLists
+            dependsOn project.exportRAWConfiguration
+
+            if (project.configuration.user_configuration.gv.use) {
+                dependsOn project.exportRAWGV
+            }
+
+            if ((project.configuration.user_configuration.settings.training.kind) &&
+                (project.configuration.user_configuration.settings.training.kind.equals("dnn")))  {
+                dependsOn project.exportRAWDNNModels
+            }
+        }
     }
 }
